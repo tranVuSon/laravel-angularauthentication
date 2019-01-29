1cb2a0e9a567a58d906fd7587e8334f42c72d30c
unknown <sontvgch16369@fpt.edu.vn>
2019-01-30 00:11:18 +0700

    commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {ResponseResetComponent} from './components/password/response-reset/response-reset.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {RequestResetComponent} from './components/password/request-reset/request-reset.component';
 
-const routes: Routes = [];
+ const appRoutes: Routes = [
+  {
+
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+
+    path: 'request-password-reset',
+    component: RequestResetComponent
+  },
+  {
+
+    path: 'response-password-reset',
+    component: ResponseResetComponent
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes)],
+    declarations: []
+
 })
 export class AppRoutingModule { }
